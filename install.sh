0f56c243d0ffe4a46d68c3839f58ee2e592705a2
Karan Saini <37518297+qurbat@users.noreply.github.com>
2023-01-08 16:50:13 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 
 if ! [ -x /usr/local/bin/massdns ] ; then
-	echo "[+] cloning from blechschmidt/massdns" && sleep 0.5
+	echo "[+] cloning blechschmidt/massdns" &&
+	sleep 0.5
 	git clone https://github.com/blechschmidt/massdns && cd massdns && echo "[+] sudo make && make install" && 
 	sleep 0.5
-	sudo make && sudo make install && sleep 0.5 && echo "[!] all done!"
+	sudo make && sudo make install && sleep 0.5 && echo "[!] all done!" &&
 	sleep 0.5
+	echo "[+] installing tldextract using pip"
+	pip install -r requirements.txt &&
 	echo "[-] cleaning up ..."
-	cd ../ && sudo rm -rf massdns/ && echo "[-] all done ... exiting"
+	cd ../ && sudo rm -rf massdns/ &&
+	echo "[-] all done ... exiting"
 fi
 
 if [ -x /usr/local/bin/massdns ] ; then
-	echo "[-] massdns binary is present in:"
+	echo "[-] the massdns binary is present in:"
 	sleep 0.5
 	which massdns
-	echo "[-] ./run.sh resources/alexa.txt"
+	sleep 0.5
+	echo "[+] installing tldextract using pip"
+	pip install -r requirements.txt &&
+	sleep 0.5
+	echo "[-] example usage: ./run.sh resources/alexa_jan_2023.txt"
 fi
 
